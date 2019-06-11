319753a271468b5f28acfc8ff9bc7127323a53c4
Mark May <mamark90@gmail.com>
2019-06-11 22:43:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,19 +12,20 @@ class Anagram
     array_match = []
     word_array = []
     array.each do |w|
+      
         if w.length == @word.length
           word_array = @word.split("")
           array_match << w.split("")
             array_match.each do |i|
               if i.sort == word_array.sort
-                binding.pry
                 @match << i.sort.join("")
+                @match
               end
             end
-          @match = []
+          else
+            @match = []
           end
         end
-    @match.join("")
   end
 end
 
