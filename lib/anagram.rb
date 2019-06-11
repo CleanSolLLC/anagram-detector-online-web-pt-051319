f41739ad21ea7f3349dbfcf752eb03220bc82da1
Mark May <mamark90@gmail.com>
2019-06-11 22:51:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,10 +11,14 @@ class Anagram
   def match(array)
     array_match = []
     word_array = []
+    #iterate over array conating strings
     array.each do |w|
-
+        #if length of word == length of word in array
+        #possible anagram
         if w.length == @word.length
+          
           word_array = @word.split("")
+          binding.pry
           array_match << w.split("")
             array_match.each do |i|
               if i.sort == word_array.sort
